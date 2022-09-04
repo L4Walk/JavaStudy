38a4c64e036083294c9ae858e927d9a415ec44c5
LJY_LU <67847083+LJYLu@users.noreply.github.com>
2022-09-04 23:07:01 +0800

    Add files via upload

---
+++ 00/Exam00.java
@@ -0,0 +1,11 @@
+// filename: Exam00.java	// 打印输出自己的姓名、班级和学号
+
+public class Exam00
+{
+	public static void main(String[] args)
+	{
+		System.out.println("姓名：卢江洋");
+		System.out.printf("班级：%s%n", "21大数据3班");
+		System.out.printf("%s：%s%n", "学号", "2100203314");
+	}
+}
